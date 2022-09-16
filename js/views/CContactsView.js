3e992affb260cfad9d061c032812788d08b8092b
nadine-shi <nadinshi@gmail.com>
2022-09-16 15:20:11 +0300

    Save own PGP key to own team contact

--- js/views/CContactsView.js
+++ js/views/CContactsView.js
@@ -1919,6 +1919,7 @@ CContactsView.prototype.addPublicPgpKey = function () {
 	};
 	Popups.showPopup(ImportKeyPopup, [{
 		allowOnlyPublicKeyForEmail: this.selectedItem().email(),
+		isOwnContact: this.selectedItem().itsMe(),
 		onSuccessCallback
 	}]);
 };
